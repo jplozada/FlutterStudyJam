cccdcbf9d3f19f75f8019d4faeaa9066063df47e
jplozada <juxnpx@gmail.com>
2019-03-29 22:34:37 -0400

    Card actualizaciones añadida

--- lib/main.dart
+++ lib/main.dart
@@ -15,37 +15,97 @@ void main() {
 }
 
 class CardLanzamiento extends StatelessWidget {
-  double paddingSize = 10.0;
-  String pathImage = "";
-
-  CardLanzamiento(this.pathImage);
-
+  String LanzamientoImage = "";
+  CardLanzamiento(this.LanzamientoImage);
   @override
   Widget build(BuildContext context) {
-
     return Container(
-        margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 130.0),
         child: Stack(
           children: <Widget>[
             Container(
             height: 120.0,
             decoration: BoxDecoration(
                 image: DecorationImage(
-                    image: AssetImage(pathImage),
+                    image: AssetImage(LanzamientoImage),
                     fit: BoxFit.cover
                 ),
                 borderRadius: BorderRadius.circular(20.0),
                 boxShadow: <BoxShadow>[
                   BoxShadow(
-                      color: Colors.black54,
+                      color: Colors.black38,
                       blurRadius: 15.0,
-                      offset: Offset(0.0, 0.75)
+                      offset: Offset(0.0, 1.0)
                   )
                 ]
               )
             )
           ],
         )
+    );
+  }
+}
+class CardActualizaciones extends StatelessWidget {
+  String ActualizacionesImage = "";
+  String ActualziacionesText = "";
+  String ActualziacionesDesc = "";
+  CardActualizaciones(this.ActualizacionesImage, this.ActualziacionesText, this.ActualziacionesDesc);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+          children: <Widget>[
+            Container(
+                  height: 110.0,
+                  width: 110.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(ActualizacionesImage),
+                          fit: BoxFit.cover
+                      ),
+                      borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(20.0),
+                          bottomLeft: const Radius.circular(20.0)
+                      ),
+                  )
+            ),
+            Positioned(
+              top: 10.0,
+              left: 115.0,
+                child: Text(
+                  ActualziacionesText,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.w500
+                  ),
+                )
+            ),
+            Positioned(
+                top: 40.0,
+                left: 115.0,
+                child: Text(
+                  ActualziacionesDesc,
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w500
+                  ),
+                )
+            )
+          ],
+        ),
+        decoration: BoxDecoration(
+          border: new Border.all(color: Colors.black),
+          color: Colors.lightBlueAccent,
+          borderRadius: BorderRadius.circular(20.0),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black54,
+              blurRadius: 15.0,
+              offset: Offset(0.0, 0.75)
+            )
+          ]
+         )
     );
   }
 }
@@ -92,7 +152,41 @@ class HelloRectangle extends StatelessWidget {
                   ),
                 ),
               ),
-              CardLanzamiento("assets/images/supermariomaker2.jpg"),
+              Positioned(
+                right: 20.0,
+                left: 20.0,
+                top: 130.0,
+                child: CardLanzamiento("assets/images/supermariomaker2.jpg"),
+              ),
+              Positioned(
+                top: 260.0,
+                right: 10.0,
+                child: Text(
+                  "Ver mas...",
+                  style: TextStyle(
+                      color: Colors.lightBlueAccent,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w300
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 280.0,
+                child: Text(
+                  "Actualizaciones",
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 26.0,
+                      fontWeight: FontWeight.w400
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                left: 20.0,
+                top: 330.0,
+                child: CardActualizaciones("assets/images/wowlogo.png", "World of Warcraft", "Parche 8.1.5"),
+              ),
             ],
           )
       );
