e1d5e5766becfa304955b19f176da5c1ababd985
jplozada <juxnpx@gmail.com>
2019-03-29 23:52:40 -0400

    ListView añadido

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class CardLanzamiento extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: EdgeInsets.only(left: 10.0, right: 20.0, top: 20.0),
         child: Stack(
           children: <Widget>[
             Container(
@@ -52,6 +53,7 @@ class CardActualizaciones extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+        margin: EdgeInsets.only(left: 10.0, right: 20.0, top: 20.0),
         child: Stack(
           children: <Widget>[
             Container(
@@ -110,85 +112,93 @@ class CardActualizaciones extends StatelessWidget {
   }
 }
 
-class HelloRectangle extends StatelessWidget {
+class ListCardActualizaciones extends StatelessWidget {
   DateTime now = DateTime.now();
   @override
   Widget build(BuildContext context) {
     String dia = DateFormat("EEEEE").format(now);
     String mes = DateFormat("LLLL y").format(now);
-    return Container(margin: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+    return ListView(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(top: 10.0),
+          child: Text(
+            '$dia, $mes',
+            style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400
+            ),
+          ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child:Text(
+              "Novedades",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w500
+              ),
+            ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child:Text(
+              "Lanzamientos",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w400
+              ),
+            ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child:CardLanzamiento("assets/images/supermariomaker2.jpg")
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child: Text(
+              "Ver mas...",
+              style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w300
+              ),
+            ),
+        ),
+        Container(
+            padding: EdgeInsets.only(top: 10.0),
+            child:Text(
+              "Actualizaciones",
+              style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.w400
+              ),
+            ),
+        ),
+        CardActualizaciones("assets/images/supermariomaker2.jpg","",""),
+        CardActualizaciones("assets/images/supermariomaker2.jpg","",""),
+        CardActualizaciones("assets/images/supermariomaker2.jpg","",""),
+        CardActualizaciones("assets/images/supermariomaker2.jpg","",""),
+        CardActualizaciones("assets/images/supermariomaker2.jpg","",""),
+      ],
+    );
+  }
+}
+
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10.0, top: 10.0),
           child: Stack(
             children: <Widget>[
-              Positioned(
-                top: 0.0,
-                child: Text(
-                  '$dia, $mes',
-                  style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w400
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 30.0,
-                child: Text(
-                  "Novedades",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.w500
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 80.0,
-                child: Text(
-                  "Lanzamientos",
-                  style: TextStyle(
-                      color: Colors.blueAccent,
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.w400
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 20.0,
-                left: 20.0,
-                top: 130.0,
-                child: CardLanzamiento("assets/images/supermariomaker2.jpg"),
-              ),
-              Positioned(
-                top: 260.0,
-                right: 10.0,
-                child: Text(
-                  "Ver mas...",
-                  style: TextStyle(
-                      color: Colors.lightBlueAccent,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w300
-                  ),
-                ),
-              ),
-              Positioned(
-                top: 280.0,
-                child: Text(
-                  "Actualizaciones",
-                  style: TextStyle(
-                      color: Colors.blueAccent,
-                      fontSize: 26.0,
-                      fontWeight: FontWeight.w400
-                  ),
-                ),
-              ),
-              Positioned(
-                right: 20.0,
-                left: 20.0,
-                top: 330.0,
-                child: CardActualizaciones("assets/images/wowlogo.png", "World of Warcraft", "Parche 8.1.5"),
-              ),
+              ListCardActualizaciones(),
             ],
-          )
+          ),
       );
   }
 }
