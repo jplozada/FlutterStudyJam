4e24c3e243d8f2cf322ba3c35f962078be3b292e
jplozada <juxnpx@gmail.com>
2019-04-21 17:08:03 -0400

    Widget TabBar añadido

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter_studyjam/widgets/text.dart';
-import 'package:flutter_studyjam/widgets/card.dart';
+import 'package:flutter_studyjam/widgets/tabs.dart';
+
 void main() {
   runApp(
     MaterialApp(
@@ -25,24 +26,8 @@ class VideoG extends StatelessWidget {
             ListView(
             children: <Widget>[
             WidgetText('$dia, $mes', Color(0xffF58299), 16, 210.0, 5.0),
-            WidgetText("News", Color(0xff29b6f6), 30.0, 125.0, 5.0),
-            WidgetText("Releases", Color(0xffEDEFA9), 26.0, 0.0, 5.0),
-            Container(
-              height: 150.0,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  WidgetCard("assets/images/smm2.png","Super Mario Maker 2","June 2019"),
-                  WidgetCard("assets/images/crash.png","Crash Team Racing Nitro Fueled","June 2019"),
-                  WidgetCard("assets/images/doom.png","Doom Eternal","December 2019"),
-                ],
-              ),
-            ),
-            WidgetText("Updates", Color(0xffEDEFA9), 26.0, 0.0, 10.0),
-            WidgetCard("assets/images/wow.png","World of Warcraft","Update 8.1.5"),
-            WidgetCard("assets/images/bf1.png","Battlefield 1","News maps"),
-            WidgetCard("assets/images/lol.png","League of Legends","New Champion!"),
-            WidgetCard("assets/images/sc2.png","Starcraft 2","Tournament soon.."),
+            WidgetText("News", Color(0xff29b6f6), 30.0, 135.0, 5.0),
+            WidgetTabs(),
             ],
           ),
         ],
---
+++ lib/widgets/tabs.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_studyjam/widgets/text.dart';
+
+Widget WidgetTabs() {
+  return Container(
+    height: 530,
+    width: double.infinity,
+    child: DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.black,
+          bottom: PreferredSize(
+            //preferredSize: Size.fromHeight(20),
+            child: Container(
+              color: Colors.transparent,
+              child: SafeArea(
+                child: Column(
+                  children: <Widget>[
+                    TabBar(
+                      isScrollable: true,
+                      labelPadding: EdgeInsets.only(top: 15),
+                      labelColor: Color(0xffEDEFA9),
+                      indicatorColor: Color(0xffEDEFA9),
+                      labelStyle: TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.w800,
+                      ),
+                      unselectedLabelColor: Color(0xffE5FFCC),
+                      unselectedLabelStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.blue
+                      ),
+                      tabs: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                          child: Text("Releases"),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                          child: Text("Updates"),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                          child: Text("Where to buy"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+           Container(
+             child: Center(
+               child: Text(
+                 "Releases",
+                 textAlign: TextAlign.center,
+                 style: TextStyle(fontSize: 15),
+               ),
+             ),
+            ),
+            Container(
+              child: Center(
+                child: Text(
+                  "Updates",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+           Container(
+             child: Center(
+               child: Text(
+                 "Where to buy",
+                 textAlign: TextAlign.center,
+                 style: TextStyle(fontSize: 15),
+               ),
+             ),
+           ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
